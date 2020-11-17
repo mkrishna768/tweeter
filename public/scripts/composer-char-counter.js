8e620ed78c7c3cba196a46685635308e18684a6c
Michael Krishna <michaelkrishna@hotmail.com>
2020-11-17 23:08:43 +0000

    created tweet styling sheet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,4 @@
-console.log("char counter loaded");
 $(document).ready(function() {
-  console.log("Document ready");
   $("#tweet-text").on("input", function(event) {
     const counter = this.nextElementSibling.lastElementChild;
     counter.innerHTML = 140 - $(this).val().length;
